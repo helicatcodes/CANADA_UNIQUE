b7a2ec1642e10d3a87bbd0bd6079e0da87cf07b4
Niels van Duijn <n.a.vanduijn@gmail.com>
2026-03-24 11:11:17 +0100

    first,last-name and display_name added to persomalise the experience

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_23_102330) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_24_100317) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -165,6 +165,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_23_102330) do
     t.date "departure_date"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.string "name"
     t.integer "program_duration"
     t.datetime "remember_created_at"
